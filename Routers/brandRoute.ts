ec6ba45524a6e06edb57bef738197715b3445530
fadihalteh <141834276+fadihalteh@users.noreply.github.com>
2024-01-16 16:53:31 +0200

    Update brandRoute.ts

--- Routers/brandRoute.ts
+++ Routers/brandRoute.ts
@@ -1,1 +1,12 @@
 
+import express from 'express';
+import {getAllBrands} from '../Controllers/brandController.js';
+const router = express.Router();
+
+router.get('/', getAllBrands);
+// router.get('/', (req,res)=>{
+//   res.json('hello world')
+// });
+
+
+export default router
