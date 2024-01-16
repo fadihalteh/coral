ba42531b7ee7cfdf1d89f6fff8828d84979d0caf
fadihalteh <141834276+fadihalteh@users.noreply.github.com>
2024-01-16 16:54:29 +0200

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import categoryRoute from './Routers/categoryRoute';
+import brandRoute from './Routers/brandRoute';
 
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json())
 app.use('/category', categoryRoute);
+app.use('/brand', brandRoute);
 
 
 app.listen(port, () => {
