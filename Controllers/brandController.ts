5005a822a7654ab1bec9f2194525dfe7f87f68be
fadihalteh <141834276+fadihalteh@users.noreply.github.com>
2024-01-16 16:52:14 +0200

    Update brandController.ts

--- Controllers/brandController.ts
+++ Controllers/brandController.ts
@@ -3,10 +3,10 @@ import { Request, Response } from 'express';
 const db = require('../Database/Models/index.ts');
 
 
-export const getAllCategories = async (_req: Request, res: Response) => {
+export const getAllBrands = async (_req: Request, res: Response) => {
     try {
-      const categories = await db.Brand.findAll();
-      return res.status(200).json(categories);
+      const brands = await db.Brand.findAll();
+      return res.status(200).json(brands);
     } catch (error) {
       console.error(error);
       return res.status(500).json({ error: 'Internal Server Error' });
