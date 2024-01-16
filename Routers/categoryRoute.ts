54df530cb00895118ec4c6a37ceda30c2e0b97ec
fadihalteh <141834276+fadihalteh@users.noreply.github.com>
2024-01-16 11:21:41 +0200

    Update categoryRoute.ts

--- Routers/categoryRoute.ts
+++ Routers/categoryRoute.ts
@@ -2,10 +2,10 @@ import express from 'express';
 import {getAllCategories} from '../Controllers/categoryController.js';
 const router = express.Router();
 
-// router.get('/', getAllCategories);
-router.get('/', (req,res)=>{
-  res.json('hello world')
-});
+router.get('/', getAllCategories);
+// router.get('/', (req,res)=>{
+//   res.json('hello world')
+// });
 
 
 export default router
