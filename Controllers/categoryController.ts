f276649443df7fcfb8651175008a0c676ad0006e
fadihalteh <fadihalteh21@gmail.com>
2024-01-16 17:15:10 +0200

    5

--- Controllers/categoryController.ts
+++ Controllers/categoryController.ts
@@ -8,7 +8,7 @@ export const getAllCategories = async (_req: Request, res: Response) => {
       return res.status(200).json(categories);
     } catch (error) {
       console.error(error);
-      return res.status(500).json({ error: 'Internal Server Error' });
+      return res.status(500).json({ error: 'Internal Server Error2' });
     }
   };
   
--- server.ts
+++ server.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import categoryRoute from './Routers/categoryRoute';
 import brandRoute from './Routers/brandRoute';
-
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(cors({
+  origin: 'https://coral-jfwb.onrender.com',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,  // enable set cookie
+}));
+
 app.use(express.json())
 app.use('/category', categoryRoute);
 app.use('/brand', brandRoute);
