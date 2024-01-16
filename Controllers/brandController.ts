dc6cbada67f476320f39125101baac4fc730017e
fadihalteh <141834276+fadihalteh@users.noreply.github.com>
2024-01-16 16:51:33 +0200

    Update brandController.ts

--- Controllers/brandController.ts
+++ Controllers/brandController.ts
@@ -1,1 +1,15 @@
 
+import { Request, Response } from 'express';
+const db = require('../Database/Models/index.ts');
+
+
+export const getAllCategories = async (_req: Request, res: Response) => {
+    try {
+      const categories = await db.Brand.findAll();
+      return res.status(200).json(categories);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  };
+  
