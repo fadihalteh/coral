9b0c1bb081f275869c591839dc998f8cfe5d90a3
fadihalteh <141834276+fadihalteh@users.noreply.github.com>
2024-01-16 11:20:07 +0200

    Update dbConfig.ts

--- Database/Config/dbConfig.ts
+++ Database/Config/dbConfig.ts
@@ -1,8 +1,19 @@
+// module.exports = {
+//     HOST: 'localhost',
+//     USER: 'fadih',
+//     PASSWORD: 'root',
+//     DB: 'coral',
+//     dialect: 'mysql',
+//     pool: {
+//       max: 10, 
+//       min: 0,  
+//     },
+//   };
 module.exports = {
-    HOST: 'localhost',
-    USER: 'fadih',
-    PASSWORD: 'root',
-    DB: 'coral',
+    HOST: 'sql11.freesqldatabase.com',
+    USER: 'sql11677209',
+    PASSWORD: 'DLqxC37xHX',
+    DB: 'sql11677209',
     dialect: 'mysql',
     pool: {
       max: 10, 
