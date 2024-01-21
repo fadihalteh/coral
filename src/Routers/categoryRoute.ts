1983aec593740bc571cfdf857278c384d2a7294f
fadihalteh <fadihalteh21@gmail.com>
2024-01-21 06:43:07 +0200

    7

--- src/Routers/categoryRoute.ts
+++ src/Routers/categoryRoute.ts
@@ -1,9 +1,11 @@
 import express from 'express';
-import {getAllCategories,getHandpickedCategories} from '../Controllers/categoryController';
+import {getAllCategories,getHandpickedCategories,getTopCategories,getMobileCategories} from '../Controllers/categoryController';
 const router = express.Router();
 
 router.get('/', getAllCategories);
 router.get('/handpicked', getHandpickedCategories);
+router.get('/top', getTopCategories);
+router.get('/mobile', getMobileCategories);
 
 
 export default router
