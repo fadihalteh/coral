85a9cc1060f5f14331e2b955c5a833100680fd2d
fadihalteh <fadihalteh21@gmail.com>
2024-01-26 19:30:42 +0200

    3

--- src/Database/Models/index.ts
+++ src/Database/Models/index.ts
@@ -1,4 +1,4 @@
-import dbConfig from "../config/dbConfig";
+import dbConfig from "../Config/dbConfig";
 import { Sequelize, DataTypes, Dialect } from "sequelize";
 
 export const sequelize = new Sequelize(
