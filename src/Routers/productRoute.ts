2d1f26683fb4c43301c0841699a120db079915a2
fadihalteh <fadihalteh21@gmail.com>
2024-01-31 17:31:14 +0200

    1

--- src/Routers/productRoute.ts
+++ src/Routers/productRoute.ts
@@ -1,15 +1,23 @@
 import express from 'express';
-import {getNewArrivals,getProducts,getLimitProducts,getDiscountPlusProducts,getPopularProducts,getProductDetails,handPickedProducts,getTrendyProducts} from '../Controllers/productController';
+import {
+  getNewArrivals,
+  getProducts,
+  getLimitProducts,
+  getDiscountPlusProducts,
+  getPopularProducts,
+  getProductDetails,
+  handPickedProducts,
+  getTrendyProducts
+} from '../Controllers/productController';
 const router = express.Router();
 
-router.get('/',getProducts);
-router.get('/new-arrivals',getNewArrivals);
-router.get('/limited-edition',getLimitProducts);
-router.get('/discount-15plus',getDiscountPlusProducts);
-router.get('/popular',getPopularProducts);
-router.get('/handpicked',handPickedProducts)
-router.get('/info/:product_id',getProductDetails)
-router.get('/trendy',getTrendyProducts)
+router.get('/new-arrivals', getNewArrivals);
+router.get('/limited-edition', getLimitProducts);
+router.get('/discount-15plus', getDiscountPlusProducts);
+router.get('/popular', getPopularProducts);
+router.get('/handpicked', handPickedProducts);
+router.get('/info/:product_id', getProductDetails);
+router.get('/trendy', getTrendyProducts);
+router.get('/', getProducts);
 
-
-export default router
+export default router;
