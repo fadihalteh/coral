8ef0431564c0ff3fcb0772c9ac974cb1f35c04b0
fadihalteh <fadihalteh21@gmail.com>
2024-01-31 13:47:24 +0200

    3

--- src/Middlewares/upload.ts
+++ src/Middlewares/upload.ts
@@ -3,7 +3,7 @@ import path from 'path';
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/'); // Set the destination folder for uploaded files
+    cb(null, 'Uploads/'); // Set the destination folder for uploaded files
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + path.extname(file.originalname)); // Set unique filename
